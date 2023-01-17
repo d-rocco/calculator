5c61607b610b2dde1b18ad67f0b80e35cda8cbc0
d-rocco <dhrocco27@gmail.com>
2023-01-17 14:15:42 -0500

    Created functions for all basic math operators

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(sum, num) {
+    return sum + num;
+}
+
+function subtract(sum, num) {
+    return sum - num;
+}
+
+function multiply(sum, num) {
+    return sum * num;
+}
+
+function divide(sum, num) {
+    if (num === 0)
+        return alert('Nice Try')
+    return sum / num;
+}
