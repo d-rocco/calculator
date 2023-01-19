f6d5e588c82ba4d41aa880c4b848882cc9d6c78f
d-rocco <dhrocco27@gmail.com>
2023-01-19 17:02:57 -0500

    Operator button functionality
    This took me awhile because the logic was confusing at first and there are a bunch of weird edge cases, but I think I dealt with everything. Now I'm going to style the calculator, make the plus/minus button functional, and also try to refactor a bit too.

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ function multiply(sum, num) {
 
 function divide(sum, num) {
     if (num === 0)
-        return alert('Nice Try')
-    return sum / num;
+        return alert('Nice Try');
+    if ((sum / num) % 1 == 0) {
+        return sum / num;
+    } else {
+        return Math.round(((sum / num) + Number.EPSILON) * 100) / 100;
+    }
 }
 
 function operate(operator, sum, num) {
@@ -23,8 +27,14 @@ function operate(operator, sum, num) {
         return subtract(sum, num);
     else if (operator === '*')
         return multiply(sum, num);
-    else 
+    else {
+        if (num === 0) {
+            alert('Nice Try');
+            clearStorage();
+            return 0;
+        } 
         return divide(sum, num);
+    }
 }
 
 let calcStorage = {
@@ -32,6 +42,8 @@ let calcStorage = {
     num: 0,
     operator: '',
     displayNum: 0,
+    isNum: false,
+    equalLastPressed: false,
 }
 // Number Buttons Functionality 
 let displayValue = [];
@@ -40,10 +52,11 @@ const display = document.querySelector('.display');
 const numberButtons = container.querySelectorAll('.num-btn');
 numberButtons.forEach((button) => {
     button.addEventListener('click', function(e) {
+        if (calcStorage.equalLastPressed) {
+            clearStorage();            
+        }
         if (displayValue.length === 9){
             alert('no space left for displaying numbers')
-        } else if (e.target.innerText === '0' && (!displayValue || !displayValue.length)) {
-            alert('cannot input 0 as first number');
         } else {
             let num = parseInt(e.target.innerText);
             displayValue.push(num);
@@ -57,9 +70,47 @@ function currentDisplay(displayValue) {
 }
 // Clear Button Functionality
 const clearButton = container.querySelector('.clear-btn');
-clearButton.addEventListener('click', function() {
-    calcStorage.sum, calcStorage.num, calcStorage.displayNum = 0;
+clearButton.addEventListener('click', clearStorage)
+
+function clearStorage() {
+    calcStorage.sum = 0;
+    calcStorage.num = 0;
+    calcStorage.displayNum = 0;
+    calcStorage.isNum = false;
+    calcStorage.equalLastPressed = false;
     displayValue = [];
     operator = '';
     display.textContent = calcStorage.displayNum;
+}
+// Operator Button Functionality
+const operatorButtons = container.querySelectorAll('.operator-btn');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', function(e) {
+        displayValue = [];
+        if (!calcStorage.isNum && !calcStorage.equalLastPressed) {
+            calcStorage.sum = calcStorage.displayNum;
+            calcStorage.isNum = true;
+            calcStorage.operator = e.target.innerText;
+            calcStorage.equalLastPressed = false;
+        } else if (e.target.innerText === '=' && calcStorage.isNum) {
+            calcStorage.num = calcStorage.displayNum;
+            calcStorage.displayNum = operate(calcStorage.operator, calcStorage.sum, calcStorage.num);
+            calcStorage.sum = calcStorage.displayNum;
+            display.textContent = calcStorage.displayNum;    
+            calcStorage.equalLastPressed = true;
+        } else {
+            if (calcStorage.equalLastPressed) {
+                calcStorage.operator = e.target.innerText;
+                calcStorage.equalLastPressed = false;
+            } else {
+                calcStorage.num = calcStorage.displayNum;
+                calcStorage.displayNum = operate(calcStorage.operator, calcStorage.sum, calcStorage.num);
+                calcStorage.operator = e.target.innerText;
+                calcStorage.sum = calcStorage.displayNum;
+                display.textContent = calcStorage.displayNum;
+                calcStorage.equalLastPressed = false;
+            }
+        }
+        console.log(calcStorage);
+    })
 })
